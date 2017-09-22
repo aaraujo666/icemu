c6e1a0d2a35a871a1cb9fa96b166f2b822c06269
Virgil Dupras <hsoft@hardcoded.net>
2017-09-21 22:07:10 -0400

    v0.2.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ LONG_DESCRIPTION = open('README.md', 'rt').read()
 
 setup(
     name='icemu',
-    version='0.1.1',
+    version='0.2.0',
     author='Virgil Dupras',
     author_email='hsoft@hardcoded.net',
     packages=['icemu'],
